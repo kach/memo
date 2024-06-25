3b2d5857b3b290b74b88e2e9170d85c5f983a2c1
kach <hardmath123@gmail.com>
2024-06-25 10:10:37 -0400

    run formatter on memoparse

--- memoparse.py
+++ memoparse.py
@@ -2,201 +2,155 @@ from memo import *
 import ast, inspect
 
 
-def parse_expr(expr : ast.expr, static_parameters: list[str]) -> Expr:
+def parse_expr(expr: ast.expr, static_parameters: list[str]) -> Expr:
     match expr:
         case ast.Constant(value=val):
             assert isinstance(val, float) or isinstance(val, int)
             return ELit(value=val)
 
-        case ast.Call(
-            func=ast.Name(id='exp'),
-            args=[e1]
-        ):
+        case ast.Call(func=ast.Name(id="exp"), args=[e1]):
+            return EOp(op=Op.EXP, args=[parse_expr(e1, static_parameters)])
+
+        case ast.Compare(left=e1, ops=[op], comparators=[e2]):
             return EOp(
-                op=Op.EXP, args=[parse_expr(e1, static_parameters)]
+                op={ast.Eq: Op.EQ, ast.Lt: Op.LT, ast.Gt: Op.GT}[op.__class__],
+                args=[
+                    parse_expr(e1, static_parameters),
+                    parse_expr(e2, static_parameters),
+                ],
             )
 
-        case ast.Compare(
-            left=e1,
-            ops=[op],
-            comparators=[e2]
-        ):
-            return EOp(
-                op={
-                    ast.Eq: Op.EQ,
-                    ast.Lt: Op.LT,
-                    ast.Gt: Op.GT
-                }[op.__class__],
-                args=[parse_expr(e1, static_parameters), parse_expr(e2, static_parameters)]
-            )
-
-        case ast.UnaryOp(
-            op=op,
-            operand=operand
-        ):
+        case ast.UnaryOp(op=op, operand=operand):
             o_expr = parse_expr(operand, static_parameters)
             return EOp(
                 op={
                     ast.USub: Op.NEG,
                     ast.Invert: Op.INV,
                 }[op.__class__],
-                args=[o_expr]
+                args=[o_expr],
             )
 
-        case ast.BinOp(
-            left=e1,
-            op=op,
-            right=e2
-        ):
+        case ast.BinOp(left=e1, op=op, right=e2):
             return EOp(
                 op={
                     ast.Add: Op.ADD,
                     ast.Sub: Op.SUB,
                     ast.Mult: Op.MUL,
-                    ast.Div: Op.DIV
+                    ast.Div: Op.DIV,
                 }[op.__class__],
-                args=[parse_expr(e1, static_parameters), parse_expr(e2, static_parameters)]
+                args=[
+                    parse_expr(e1, static_parameters),
+                    parse_expr(e2, static_parameters),
+                ],
             )
 
-        case ast.BoolOp(
-            op=op,
-            values=values
-        ):
+        case ast.BoolOp(op=op, values=values):
             if len(values) != 2:
-                raise Exception(f"Incorrect number of arguments to logical operator {op}")
+                raise Exception(
+                    f"Incorrect number of arguments to logical operator {op}"
+                )
             e1, e2 = values
             return EOp(
                 op={ast.And: Op.AND, ast.Or: Op.OR}[op.__class__],
-                args = [parse_expr(e1, static_parameters),
-                        parse_expr(e2, static_parameters)]
+                args=[
+                    parse_expr(e1, static_parameters),
+                    parse_expr(e2, static_parameters),
+                ],
             )
 
-        case ast.IfExp(
-                test=test,
-                body=body,
-                orelse=orelse
-        ):
+        case ast.IfExp(test=test, body=body, orelse=orelse):
             c_expr = parse_expr(test, static_parameters)
             t_expr = parse_expr(body, static_parameters)
             f_expr = parse_expr(orelse, static_parameters)
             return EOp(op=Op.ITE, args=[c_expr, t_expr, f_expr])
-
 
         case ast.Name(id=id):
             if id in static_parameters:
                 return ELit(id)
             return EChoice(id=Id(id))
 
-        case ast.Subscript(
-            value=ast.Name(id='E'),
-            slice=slice
-        ):
+        case ast.Subscript(value=ast.Name(id="E"), slice=slice):
             assert not isinstance(slice, ast.Slice)
             assert not isinstance(slice, ast.Tuple)
             return EExpect(expr=parse_expr(slice, static_parameters))
 
-        case ast.Subscript(
-                value=ast.Name("imagine"),
-                slice=ast.Tuple(
-                    elts=elts
-                )
-        ):
+        case ast.Subscript(value=ast.Name("imagine"), slice=ast.Tuple(elts=elts)):
             stmts = []
             for elt in elts[:-1]:
                 match elt:
                     case ast.Slice(
-                        lower=ast.Name(id=who_),
-                        upper=expr_,
-                        step=None
+                        lower=ast.Name(id=who_), upper=expr_, step=None
                     ) if expr_ is not None:
                         stmts.extend(parse_stmt(expr_, who_, static_parameters))
             assert not isinstance(elts[-1], ast.Slice)
             return EImagine(do=stmts, then=parse_expr(elts[-1], static_parameters))
 
-        case ast.Subscript(
-            value=ast.Name(id=who_id),
-            slice=slice
-        ):
+        case ast.Subscript(value=ast.Name(id=who_id), slice=slice):
             assert not isinstance(slice, ast.Slice)
             assert not isinstance(slice, ast.Tuple)
             return EWith(who=Name(who_id), expr=parse_expr(slice, static_parameters))
-        case ast.Attribute(
-                value = ast.Name(id=who_id),
-                attr=attr
-                ):
+        case ast.Attribute(value=ast.Name(id=who_id), attr=attr):
             return EWith(who=Name(who_id), expr=EChoice(attr))
 
         case _:
             raise Exception(f"Unknown expression {expr} at line {expr.lineno}")
 
 
-def parse_stmt(expr : ast.expr, who : str, static_parameters: list[str]) -> list[Stmt]:
+def parse_stmt(expr: ast.expr, who: str, static_parameters: list[str]) -> list[Stmt]:
     match expr:
         case ast.Call(
-            func=ast.Name(id='chooses'),
+            func=ast.Name(id="chooses"),
             args=[
                 ast.Compare(
                     left=ast.Name(id=choice_id),
                     comparators=[ast.Name(id=dom_id)],
-                    ops=[ast.In()]
+                    ops=[ast.In()],
                 )
             ],
-            keywords=[ast.keyword(arg='wpp', value=wpp_expr)]
+            keywords=[ast.keyword(arg="wpp", value=wpp_expr)],
         ):
-            return [SChoose(
-                who=Name(who),
-                id=Id(choice_id),
-                domain=dom_id,
-                wpp=parse_expr(wpp_expr, static_parameters)
-            )]
+            return [
+                SChoose(
+                    who=Name(who),
+                    id=Id(choice_id),
+                    domain=dom_id,
+                    wpp=parse_expr(wpp_expr, static_parameters),
+                )
+            ]
         case ast.Compare(
             left=ast.Subscript(
                 value=ast.Name(id="observes"),
                 slice=ast.Attribute(
                     value=ast.Name(id=target_who),
                     attr=target_id,
-                )
+                ),
             ),
             ops=[ast.Is()],
-            comparators=[
-                ast.Attribute(
-                    value=ast.Name(id=source_who),
-                    attr=source_id
+            comparators=[ast.Attribute(value=ast.Name(id=source_who), attr=source_id)],
+        ):
+            return [
+                SShow(
+                    who=Name(who),
+                    target_who=Name(target_who),
+                    target_id=Id(target_id),
+                    source_who=Name(source_who),
+                    source_id=Id(source_id),
                 )
             ]
-        ):
-            return [SShow(
-                who=Name(who),
-                target_who=Name(target_who),
-                target_id=Id(target_id),
-                source_who=Name(source_who),
-                source_id=Id(source_id)
-            )]
         case ast.Subscript(
-            value=ast.Name('thinks'),
-            slice=ast.Slice(
-                lower=ast.Name(who_),
-                upper=expr_
-            )
+            value=ast.Name("thinks"), slice=ast.Slice(lower=ast.Name(who_), upper=expr_)
         ) if expr_ is not None:
-            return [SWith(
-                who=Name(who),
-                stmt=s
-            ) for s in parse_stmt(expr_, who_, static_parameters)]
-        case ast.Subscript(
-            value=ast.Name('thinks'),
-            slice=ast.Tuple(
-                elts=elts
-            )
-        ):
+            return [
+                SWith(who=Name(who), stmt=s)
+                for s in parse_stmt(expr_, who_, static_parameters)
+            ]
+        case ast.Subscript(value=ast.Name("thinks"), slice=ast.Tuple(elts=elts)):
             stmts = []
             for elt in elts:
                 match elt:
                     case ast.Slice(
-                            lower=ast.Name(id=who_),
-                            upper=expr_,
-                            step=None
-                        ) if expr_ is not None:
+                        lower=ast.Name(id=who_), upper=expr_, step=None
+                    ) if expr_ is not None:
                         stmts.extend(parse_stmt(expr_, who_, static_parameters))
                     case _:
                         raise Exception()
@@ -223,11 +177,9 @@ def memo(f) -> None:
             first_stmt = f.body[0]
             match first_stmt:
                 case ast.AnnAssign(
-                    target=ast.Name(id='cast'),
-                    annotation=ast.List(
-                        elts=elts
-                    ),
-                    value=None
+                    target=ast.Name(id="cast"),
+                    annotation=ast.List(elts=elts),
+                    value=None,
                 ):
                     for elt in elts:
                         assert isinstance(elt, ast.Name)
@@ -243,29 +195,20 @@ def memo(f) -> None:
         # print(ast.dump(stmt, include_attributes=True, indent=2))
         match stmt:
             case ast.AnnAssign(
-                target=ast.Name(id='forall'),
+                target=ast.Name(id="forall"),
                 annotation=ast.Compare(
                     left=ast.Name(id=choice_id),
                     comparators=[ast.Name(id=dom_id)],
-                    ops=[ast.In()]
+                    ops=[ast.In()],
                 ),
-                value=None
+                value=None,
             ):
                 assert choice_id not in static_parameters
-                stmts.append(SForAll(
-                    id=Id(choice_id),
-                    domain=dom_id
-                ))
-            case ast.AnnAssign(
-                target=ast.Name(id=who),
-                annotation=expr,
-                value=None
-            ):
+                stmts.append(SForAll(id=Id(choice_id), domain=dom_id))
+            case ast.AnnAssign(target=ast.Name(id=who), annotation=expr, value=None):
                 assert who in cast
                 stmts.extend(parse_stmt(expr, who, static_parameters))
-            case ast.Return(
-                value=expr
-            ) if expr is not None:
+            case ast.Return(value=expr) if expr is not None:
                 if retval is not None:
                     raise Exception()
                 retval = parse_expr(expr, static_parameters)
@@ -273,10 +216,7 @@ def memo(f) -> None:
                 raise Exception()
 
     if retval is None:
-        raise Exception()
-    # for s in stmts:
-        # print(pprint_stmt(s))
-    # print(pprint_expr(retval))
+        raise Exception("You didn't return anything!")
 
     io = StringIO()
     ctxt = Context(next_idx=0, io=io, frame=Frame(name=Name("root")), idx_history=[])
@@ -284,20 +224,22 @@ def memo(f) -> None:
     for stmt in stmts:
         eval_stmt(stmt, ctxt)
     val = eval_expr(retval, ctxt)
-    ctxt.emit(f'return {val.tag}')
+    ctxt.emit(f"return {val.tag}")
 
-    out = '''def _out(''' + ', '.join(static_parameters) + '):\n' + textwrap.indent(io.getvalue(), '    ')
+    out = (
+        """def _out("""
+        + ", ".join(static_parameters)
+        + "):\n"
+        + textwrap.indent(io.getvalue(), "    ")
+    )
 
+    # for s in stmts:
+    #     print(pprint_stmt(s))
+    # print(pprint_expr(retval))
     # for i, line in enumerate(out.splitlines()):
-        # print(f"{i + 1: 5d}  {line}")
+    #     print(f"{i + 1: 5d}  {line}")
 
     globals_of_caller = inspect.stack()[1].frame.f_globals
     retvals: dict[Any, Any] = {}
     exec(out, globals_of_caller, retvals)
-    return retvals['_out']
-
-    # print(retvals)
-    # print(retvals['listener_ll_9'], retvals['listener_ll_9'].shape)
-    # print(ctxt.idx_history)
-    # print(retvals['u_ll_28'], retvals['u_ll_28'].shape)
-    # print(retvals['speaker_r_0'], retvals['speaker_r_0'].shape)
+    return retvals["_out"]
