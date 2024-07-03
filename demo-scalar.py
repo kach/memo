55717ad4f36b987ff68170bffdbeaf0b67435a80
kach <hardmath123@gmail.com>
2024-07-03 00:10:18 -0400

    use new stx in scalar

--- demo-scalar.py
+++ demo-scalar.py
@@ -6,16 +6,12 @@ N = [0, 1, 2, 3]  # number of nice people
 U = [0, 1, 2]     # utterance: {none, some, all} of the people are nice
 
 @jax.jit
-def meaning(n, u):
-    #                 (none)  (some)  (all)
+def meaning(n, u):  # (none)  (some)  (all)
     return np.array([ n == 0, n > 0, n == 3 ])[u]
 
 @memo
-def scalar():
+def scalar[n: N, u: U]():
     cast: [speaker, listener]
-    forall: n in N
-    forall: u in U
-
     listener: thinks[
         speaker: given(n in N, wpp=1),
         speaker: chooses(u in U, wpp=imagine[
@@ -26,7 +22,6 @@ def scalar():
     ]
     listener: hears [speaker.u] is u
     listener: chooses(n in N, wpp=E[speaker.n == n])
-
     return E[listener.n == n]
 
 print(scalar())
