1cfdb1162beea779d2fb888a8ecc034a03e6c6d4
kach <hardmath123@gmail.com>
2024-07-03 00:12:33 -0400

    rsa recursive swap speaker/listener

--- demo-rsa-recursive.py
+++ demo-rsa-recursive.py
@@ -14,17 +14,6 @@ def denotes(u, r):
     ])[r, u]
 
 @memo
-def listener[u: U, r: R](beta, t):
-    cast: [listener, speaker]
-    listener: thinks[
-        speaker: given(r in R, wpp=1),
-        speaker: chooses(u in U, wpp=speaker[u, r](beta, t - 1) if t > 0 else denotes(u, r))
-    ]
-    listener: observes [speaker.u] is u
-    listener: chooses(r in R, wpp=E[speaker.r == r])
-    return E[listener.r == r]
-
-@memo
 def speaker[u: U, r: R](beta, t):
     cast: [speaker, listener]
     speaker: knows(r)
@@ -34,6 +23,17 @@ def speaker[u: U, r: R](beta, t):
         exp(beta * E[listener.r == r])
     ])
     return E[speaker.u == u]
+
+@memo
+def listener[u: U, r: R](beta, t):
+    cast: [listener, speaker]
+    listener: thinks[
+        speaker: given(r in R, wpp=1),
+        speaker: chooses(u in U, wpp=speaker[u, r](beta, t - 1) if t > 0 else denotes(u, r))
+    ]
+    listener: observes [speaker.u] is u
+    listener: chooses(r in R, wpp=E[speaker.r == r])
+    return E[listener.r == r]
 
 beta = 3.
 for t in range(10):
