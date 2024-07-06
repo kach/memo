9f13520df260ac146a815b7e6e2fcb2cd4374563
kach <hardmath123@gmail.com>
2024-07-06 18:55:27 -0400

    tinkering with err demo

--- demo-err.py
+++ demo-err.py
@@ -3,27 +3,16 @@ X = [1, 2, 3]
 Y = [2, 3]
 
 @memo
-def f[y: X]():
+def f[x: X]():
     cast: [alice, bob]
-    bob: thinks [ alice: chooses(x in X, wpp=1) ]
-    bob: observes [alice.x] is y
-    return bob[ alice.x ]
+    alice: knows(x)
+    alice: thinks[ bob: knows(x) ]
+    return alice[bob[x]]
+print(f())
 
 # @memo
-# def f():
+# def f[y: X]():
 #     cast: [alice, bob]
-#     alice: chooses(x in X, wpp=1)
 #     bob: thinks [ alice: chooses(x in X, wpp=1) ]
-#     alice: thinks [ bob: knows(y) ]
-#     return 1
-
-# @memo
-# def f():
-#     cast: [alice, bob]
-#     alice: chooses(x in X, wpp=1)
-#     alice: thinks[
-#         bob: chooses(y in Y, wpp=1)
-#     ]
-#     alice: observes [bob.y] is alice.x
-#     return E[alice.x - alice[bob.y]]
-# print(f())
+#     bob: observes [alice.x] is y
+#     return bob[ alice.x ]
