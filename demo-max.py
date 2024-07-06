9c5afe1538409a97fb6b0db0c0b05b1aa3f8b5b5
tony chen <tchen1998@gmail.com>
2024-07-06 16:31:34 -0400

    fix formatting with demo

--- demo-max.py
+++ demo-max.py
@@ -3,11 +3,13 @@ from memo import memo
 X = [1, 2]
 Y = [0, 1, 2]
 
+
 @memo
 def f():
     cast: [alice]
-    alice: chooses(x in X, to_maximize=x)
+    alice: chooses(x in X, to_maximize=x, wpp=x)
     return E[alice.x]
+
 
 @memo
 def g():
@@ -16,9 +18,15 @@ def g():
     alice: chooses(y in Y, to_maximize=self.x * y)
     return E[alice.y]
 
+
 @memo
 def h():
     cast: [alice]
     alice: chooses(x in X, wpp=1)
     alice: chooses(y in Y, to_maximize=1.0 * (self.x + y < 4) * (self.x + y))
     return E[alice.y]
+
+
+f()
+g()
+h()
