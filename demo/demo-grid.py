53201de16d2d0b50c356b16d47e76bf33e77bde3
kach <hardmath123@gmail.com>
2024-10-09 16:36:49 -0400

    update grid to tonys version

--- demo/demo-grid.py
+++ demo/demo-grid.py
@@ -10,10 +10,11 @@ sys.setrecursionlimit(1_000_000)
 
 from memo import memo
 
-H = 10
-W = 5
+H = 21
+W = 21
 S = np.arange(H * W)
-G = np.array([0, 4])
+G = np.array([0, H * W - 1])
+# G = np.array([130, H * W - 130])
 A = np.array([0, 1, 2, 3])  # left, right, up, down
 
 coord_actions = np.array([
@@ -24,18 +25,8 @@ coord_actions = np.array([
 ])
 
 # fmt: off
-maze = np.array([
-    0, 0, 0, 0, 0,
-    0, 1, 0, 1, 0,
-    0, 1, 1, 1, 0,
-    0, 1, 0, 1, 0,
-    0, 1, 0, 1, 0,
-    0, 1, 0, 1, 0,
-    0, 1, 0, 1, 0,
-    0, 1, 0, 0, 0,
-    0, 1, 0, 0, 0,
-    0, 0, 0, 0, 0,
-])
+maze_raw = np.array(1 - plt.imread('../paper/fig/logo-maze.png'), dtype=int); maze = maze_raw.reshape(-1)
+# maze = np.zeros(H * W)
 # fmt: on
 
 
@@ -58,120 +49,44 @@ def Tr(s, a, s_):
 @jax.jit
 def R(s, a, g):
     """Reward of 1 for being on the goal state."""
-    return 1.0 * (s == g)
+    return 1.0 * (s == g) - 0.1
 
 
 @jax.jit
 def is_terminating(s, g):
     return s == g
 
+@jax.jit
+def gamma():
+    return 1.0
 
 @cache
 @memo
-def V[s: S, g: G](t):
-    cast: [alice]
-    alice: knows(s)
-    alice: knows(g)
-
-    # alice chooses her action based on her policy
-    alice: chooses(a in A, wpp=π[s, a, g](t))
-
-    # alice gets the next state
+def Q[s: S, a: A, g: G](t):
+    alice: knows(s, a, g)
     alice: given(s_ in S, wpp=Tr(s, a, s_))
-
-    # her value depends on the expected V-function at the next state
+    alice: chooses(a_ in A, to_maximize=0.0 if t < 0 else Q[s_, a_, g](t - 1))
     return E[
-        R(s, alice.a, g) +
-        (0.0 if t < 0 else (0.0 if is_terminating(s, g) else 0.9 * V[alice.s_, g](t - 1)))
+        R(s, a, g) + (0.0 if t < 0 else 0.0 if is_terminating(s, g) else gamma() * Q[alice.s_, alice.a_, g](t - 1))
     ]
 
-
-@cache
 @memo
-def π[s: S, a: A, g: G](t):
-    cast: [alice]
-    alice: knows(s)
-    alice: knows(g)
-
-    # alice chooses her action based on a softmax over future value
-    alice: chooses(
-        a in A,
-        wpp=exp(
-            2.0 * (R(s, a, g) + (0.0 if t < 0 else ( 0.0 if is_terminating(s, g) else
-                0.9 * imagine[
-                            future_alice: given(s_ in S, wpp=Tr(s, a, s_)),
-                            E[V[future_alice.s_, future_alice.g](t - 1)],
-                        ]
-                    )
-                )
-            )
-        ),
-    )
-    return E[ alice.a == a ]
-
-
-@memo
-def invplan[s: S, a: A]():
-    cast: [observer, alice]
-    observer: knows(a)
-    observer: knows(s)
-
+def invplan[s: S, a: A, g: G](t):
+    observer: knows(a, s, g)
     observer: thinks[
         alice: chooses(g in G, wpp=1),
         alice: knows(s),
-        alice: chooses(a in A, wpp=π[s, a, g](200)),
+        alice: chooses(a in A, wpp=exp(2 * Q[s, a, g](t))),
     ]
     observer: observes [alice.a] is a
-    return observer[E[alice.g == 0]]
+    return observer[E[alice.g == g]]
 
 if len(sys.argv) > 1:
+    import time
     t = int(sys.argv[1])
     print('t =', t)
-    v = V(t)
-    print(v[1, 1])
+    Q(0);
+    t0 = time.time()
+    v = Q(t).max(axis=1)
+    print(time.time() - t0)
     exit()
-
-t = 100
-
-plt.subplot(1, 2, 1)
-value_fn = V(t).transpose().reshape((2, H, W))[0]
-
-ic(value_fn)
-p = plt.imshow(
-    value_fn * (1 - maze.reshape((H, W))),
-    origin="upper",
-    cmap="PuRd_r",
-)
-plt.colorbar(p)
-
-policy = π(t).transpose().reshape(2, 4, H, W)[0]
-policy = policy.argmax(axis=0)
-
-directions = coord_actions[policy]
-plt.quiver(
-    np.arange(W),
-    np.arange(H),
-    directions[:, :, 0],
-    -directions[:, :, 1],
-    color="red",
-)
-plt.axis("off")
-
-
-plt.subplot(1, 2, 2)
-posterior = invplan().transpose()
-plt.imshow(
-    1 - maze.reshape((H, W)),
-    origin="upper",
-    cmap="gray",
-)
-
-plt.quiver(np.arange(W), np.arange(H), -np.ones((H, W)), np.zeros((H, W)), posterior[0].reshape((H, W)), cmap="coolwarm", clim=(0, 1))
-plt.quiver(np.arange(W), np.arange(H), np.ones((H, W)), np.zeros((H, W)), posterior[1].reshape((H, W)), cmap="coolwarm", clim=(0, 1))
-plt.quiver(np.arange(W), np.arange(H), np.zeros((H, W)), np.ones((H, W)), posterior[2].reshape((H, W)), cmap="coolwarm", clim=(0, 1))
-plt.quiver(np.arange(W), np.arange(H), np.zeros((H, W)), -np.ones((H, W)), posterior[3].reshape((H, W)), cmap="coolwarm", clim=(0, 1))
-
-plt.gca().add_patch(plt.Rectangle((-0.5, -0.5), W, H, fill="tab:gray", ec="black", linewidth=5, alpha=0.25))
-
-plt.axis("off")
-plt.savefig('out.png')
