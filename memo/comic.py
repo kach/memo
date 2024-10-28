77faf3d49b57509e996da0805cdc73b977e63b12
kach <hardmath123@gmail.com>
2024-10-28 10:02:05 -0400

    remove val arg from comic

--- memo/comic.py
+++ memo/comic.py
@@ -7,17 +7,17 @@ def frame_name(frame):
 def node_name(frame, who, id_):
     return f'{who}_{id_}_{id(frame)}'
 
-def comic_frame_edges(frame: Frame, retval: Value, io: StringIO) -> None:
+def comic_frame_edges(frame: Frame, io: StringIO) -> None:
     for c in frame.children.keys():
-        comic_frame_edges(frame.children[c], retval, io)
+        comic_frame_edges(frame.children[c], io)
 
     if frame.parent is not None:
         for too, frm in frame.conditions.items():
             print(f'{node_name(frame.parent, *frm)} -> {node_name(frame, *too)}[style="dashed"];', file=io)
 
-def comic_frame_nodes(frame: Frame, retval: Value, io: StringIO) -> None:
+def comic_frame_nodes(frame: Frame, io: StringIO) -> None:
     for c in frame.children.values():
-        comic_frame_nodes(c, retval, io)
+        comic_frame_nodes(c, io)
 
     print(f'subgraph {frame_name(frame)} {{', file=io)
     print(f'label="{frame.name}\'s frame";', file=io)
@@ -29,17 +29,18 @@ def comic_frame_nodes(frame: Frame, retval: Value, io: StringIO) -> None:
 
     for (who, id), ch in frame.choices.items():
         color = "lightblue" if ch.known else "orange"
-        print(f'''{node_name(frame, who, id)}[label="{who}.{id} : {ch.domain}", color={color}];''', file=io)
+        label = f'{who}.{id}' if who != 'self' else f'{id}'
+        print(f'''{node_name(frame, who, id)}[label="{label} : {ch.domain}", color={color}];''', file=io)
 
     print('}', file=io)
 
-def comic(frame: Frame, retval: Value, fname: str) -> None:
+def comic(frame: Frame, fname: str) -> None:
     io = StringIO()
     print('digraph G {', file=io)
     print('rankdir=LR; compound=true;', file=io)
     print('node[shape="cds", style="filled"];', file=io)
-    comic_frame_nodes(frame, retval, io)
-    comic_frame_edges(frame, retval, io)
+    comic_frame_nodes(frame, io)
+    comic_frame_edges(frame, io)
     print('}', file=io)
 
     with open(f'{fname}.dot', 'w') as f:
