17a7ad985b1c60571b4961997a6024591829a84d
kach <hardmath123@gmail.com>
2024-10-28 01:02:19 -0400

    add eig demo

---
+++ demo/demo-eig.py
@@ -0,0 +1,57 @@
+"""
+Question-asking based on expected information gain (EIG)
+
+Bob rolls a red die and a blue die.
+Alice gets to ask one yes-no question about the sum.
+What is the most informative question she could ask,
+in order to learn the most about the two die rolls?
+
+We'll compute the EIG of various questions...
+"""
+
+from memo import memo
+import jax
+import jax.numpy as np
+
+is_prime = np.array([0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0])
+Qs = [
+    lambda n: n == 7,
+    lambda n: n == 12,
+    lambda n: n > 10,
+    lambda n: n > 8,
+    lambda n: n > 6,
+    lambda n: n > 5,
+    lambda n: n % 2 == 0,
+    lambda n: n % 3 == 0,
+    lambda n: n % 4 == 0,
+    lambda n: n % 5 == 0,
+    lambda n: is_prime[n],
+]
+
+N = np.arange(1, 6 + 1)  # single die's outcomes
+Q = np.arange(len(Qs))   # questions
+A = np.array([0, 1])     # answers (yes/no)
+
+@jax.jit
+def respond(q, a, n):
+    return np.array([q_(n) for q_ in Qs])[q] == a
+
+@memo
+def eig[q: Q]():
+    alice: knows(q)
+    alice: thinks[
+        bob: knows(q),
+        bob: chooses(n_red in N, wpp=1),
+        bob: chooses(n_blu in N, wpp=1),
+        bob: chooses(a in A, wpp=respond(q, a, n_red + n_blu))
+    ]
+    return alice[ H[bob.n_red, bob.n_blu] - imagine[
+        future_alice: observes [bob.a] is bob.a,
+        E[future_alice[ H[bob.n_red, bob.n_blu] ]]
+    ] ]
+
+z = eig()
+import inspect
+q_names = [inspect.getsource(q_).strip()[10:-1] for q_ in Qs]
+for eig_, q_ in reversed(sorted(list(zip(z, q_names)))):
+    print(f'{eig_:0.5f}', q_)
